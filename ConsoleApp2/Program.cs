c79ecc10ff2b489c04b4463fe87264dc24e2b02e
MD Rezaul Siddiqui <rezaul.siddiqui.spo16@ecusolna.onmicrosoft.com>
2017-09-25 11:01:16 +0200

    hi

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            var age = int.Parse(Console.ReadLine());
+            if (age < 16)
+            {
+                Console.WriteLine("You seem to be young.");
+
+            }
+
+            else if (age< 18)
+                {
+
+                    Console.WriteLine("Maybe study on gynisiaum?");
+                
+                }
+
+            else
+            {
+
+                Console.WriteLine{ "Maybe YH?"}
+            }
+
+
+
+        }
+    }
+}
